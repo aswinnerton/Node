8a7b33a8260e0096d636c58300f89d7fb556067d
aswinnerton <alexander.ds@live.com>
2017-11-06 17:22:14 +0000

    misery update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import { getSearchAndOutput } from "./recipe.js";
 
+function clicker() {
+    const ing = document.getElementById("ingredientSearch").value;
+    const sear = document.getElementById("searchQuery").value;
 
+    const inputID = document.getElementById("pokeIDInput").value;
+    getSearchAndOutput(ing,sear);
+}
--- src/recipe.js
+++ src/recipe.js
@@ -1,12 +1,53 @@
 
 "use strict";
+const url = "http://www.recipepuppy.com/api/?";
 
-function getIngredients() {
-    let ingSearch = document.getElementById("ingredientSearch").value;
+//example: http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3
 
-
+async function getIngredients(ingSearch, foodSearch) {
+    const response = await fetch(`${url}i=${ingSearch}&q=${foodSearch}`);
+    const data = await response.json();
+    return data;
 }
 
-function getSearchQuery() {
-    let foodSearch = document.getElementById("searchQuery").value;
+function ifError(er) {
+    document.getElementById("ingredientSearch").placeholder = "Enter a valid ingredient";
+    document.getElementById("ingredientSearch").value = "";
+    document.getElementById("searchQuery").placeholder = "Enter a valid food title";
+    document.getElementById("searchQuery").value = "";
+}
+
+function clearData() {
+    document.getElementById("testOutput").innerHTML = "";
+}
+
+export function getSearchAndOutput(ingredient, title) {
+
+    clearData();
+
+    let foodPromise = Promise.resolve(getIngredients(ingredient, title))
+    foodPromise.then((jsonObj) => {
+        const foodObject = transformRecipe(jsonObj);
+        document.getElementById("resultsTable").innerHTML = `
+          <tr>
+            <td>${foodObject.title}</td>
+            <td>${foodObject.ingredients}</td>          
+          </tr>       
+        `;
+        document.getElementById("ingredientSearch").placeholder = "Enter a ingredient";
+        document.getElementById("searchQuery").placeholder = "Enter a food title";
+
+    }).catch(ifError);
+}
+
+function transformRecipe(jsonFoodObj) {
+    const foodObject = {};
+
+    for (let results in jsonFoodObj) {
+        for (let entry in results) {
+            foodObject.title += jsonFoodObj.title;
+            foodObject.ingredients += jsonFoodObj.ingredients;
+        }
+    }
+    return foodObject;
 }
