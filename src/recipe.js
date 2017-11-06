4c15f1d236759d22ab37ee53eab2644c5f179773
aswinnerton <alexander.ds@live.com>
2017-11-06 15:55:31 +0000

    updated

---
+++ src/recipe.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+function getIngredients() {
+    let ingSearch = document.getElementById("ingredientSearch").value;
+
+
+}
+
+function getSearchQuery() {
+    let foodSearch = document.getElementById("searchQuery").value;
+}
